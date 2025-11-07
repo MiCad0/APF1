ee269873de03c2b76433b1866c57ce512e9ff5ba
SaadRoty <116813450+SaadRoty@users.noreply.github.com>
2025-11-07 10:57:42 +0100

    addcomments

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, jsonify
 
+#Creer l'app flask
 app = Flask(__name__)
+
+# Initialiser SQLAlchemy
+db = SQLAlchemy(app)
+
 
 
 @app.route('/')
 def home():
     return 'Hello World!'
 
-
+#Activer mode Debug pour voir les erreur et recharger automatiquement le serveur
 if __name__ == '__main__':
     app.run(debug=True)
