fee5f2528ed11fbd537d544d8f6500e571038869
MiCad0 <denislinde4@gmail.com>
2025-11-07 10:39:19 +0100

    added default route

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return '<a href="'+request.path+'students"> Hello World!</a>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
