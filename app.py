a33cd6a5d168fe98fdd12f7e1ee54b66e92eefa0
SaadRoty <116813450+SaadRoty@users.noreply.github.com>
2025-11-07 11:24:54 +0100

    add methode 2-5

--- app.py
+++ app.py
@@ -26,15 +26,43 @@ def home():
 
 #Ajouter un nouvel etudiant (methode POST)
 @app.route('/students', methods=['POST'])
+def add_student():
+    data = request.get_json()
+    new_student = Student(name=data['name'], age=data['age'])
+    db.session.add(new_student)
+    db.session.commit()
+    return jsonify(new_student.to_dict()), 201
 
 #Affichier un étudiant sachant son ID
 @app.route('/students/<int:id>', methods=['GET'])
+def get_student(id):
+    student = Student.query.get(id)
+    if student:
+        return jsonify(student.to_dict())
+    return jsonify({'error': 'Etudiant non trouvé'}), 404
 
 #Mettre a jour un etudiant (methode PUT)
 @app.route('/students/<int:id>', methods=['PUT'])
+def update_student(id):
+    student = Student.query.get(id)
+    if not student:
+        return jsonify({'error': 'Etudiant non trouvé'}), 404
+    
+    data = request.get_json()
+    student.name = data.get('name', student.name)
+    student.age = data.get('age', student.age)
+    db.session.commit()
+    return jsonify(student.to_dict())
 
 #Supprimer un etudiant (methode DELETE)
 @app.route('/students/<int:id>', methods=['DELETE'])
+def delete_student(id):
+    student = Student.query.get(id)
+    if not student:
+        return jsonify({'error': 'Etudiant non trouvé'}), 404
+    db.session.delete(student)
+    db.session.commit()
+    return jsonify({'message': 'Etudiant supprimé'}), 200
 
 
 
