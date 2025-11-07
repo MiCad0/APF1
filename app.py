75d13f32c774f2e866764422c3d1630b19505cb6
SaadRoty <116813450+SaadRoty@users.noreply.github.com>
2025-11-07 11:03:23 +0100

    initialise BDD

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, request, jsonify
 #Creer l'app flask
 app = Flask(__name__)
 
+#Confi de la BDD
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # Initialiser SQLAlchemy
 db = SQLAlchemy(app)
-
 
 
 @app.route('/')
